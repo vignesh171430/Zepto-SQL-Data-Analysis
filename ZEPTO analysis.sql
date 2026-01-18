274aa58bb3c69863b4e9865f5e65c31713eadf49
Vignesh S <vickysuresh298@gmail.com>
2026-01-19 00:42:53 +0530

    Add files via upload

---
+++ ZEPTO analysis.sql
@@ -0,0 +1,14 @@
+drop table if exists zepto;
+
+create table zepto (
+sku_id SERIAL PRIMARY KEY,
+category VARCHAR(120),
+name VARCHAR(150) NOT NULL,
+mrp NUMERIC(8,2),
+discountPercent NUMERIC(5,2),
+availableQuantity INTEGER,
+discountedSellingPrice NUMERIC(8,2),
+weightInGms INTEGER,
+outOfStock BOOLEAN,	
+quantity INTEGER
+);
